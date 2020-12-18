0c72245fc940421c8bfc67b8ab67be427fb316d8
xc <xuchen@eenet.com>
2020-12-19 01:22:00 +0800

    la

---
+++ LocalOs.py
@@ -0,0 +1,11 @@
+
+class myLocal(object):
+    def __init__(self,muk):
+        self.name ="xuchen"
+        self.age = 29
+        self.lovf = "ujm"
+        self.ty = muk
+
+    def get(self):
+        print(self.name)
+
